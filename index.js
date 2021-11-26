21af0875640e508546026f3e19bcfebc63dbff43
Maya Comfort <mdcomfort@gmail.com>
2021-11-26 00:16:11 -0500

    core lab completed

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 console.log("Hello World")
 
+const message = document.querySelector('#message')
+
 function addMovie(event) {
     event.preventDefault()
+
     const inputField = document.querySelector('input')
     const movie = document.createElement('li')
     const movieTitle = document.createElement('span')
     movieTitle.textContent = inputField.value
+    movieTitle.addEventListener('click', crossOffMovie)
     movie.appendChild(movieTitle)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
     document.querySelector('ul').appendChild(movie)
+
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
+
+
+function deleteMovie(event) {
+    event.preventDefault()
+    event.target.parentNode.remove()
+    message.textContent = "Movie deleted!"
+}
+
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
+
+    if (event.target.classList.contains('checked')) {
+        message.textContent = "Movie watched!"
+    } else {
+        message.textContent = "Movie added back!"
+    }
+}
